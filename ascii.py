13d357ad623e148f56061f2bc0c26a507912d232
arpitpandey992 <arpitpandey992@gmail.com>
2022-06-17 15:22:40 +0530

    Commit 2

--- ascii.py
+++ ascii.py
@@ -34,7 +34,7 @@ def PIL2CV2(image):
     return cv2_img
 
 
-char_list, font = get_data("complex", 13)
+char_list, font = get_data(mode="complex", fontSize=6)
 
 
 def AscifyImage(image, bg="black", res=-1, save=False, show=False):
@@ -47,9 +47,13 @@ def AscifyImage(image, bg="black", res=-1, save=False, show=False):
         scale = image_width/res
         image_width = res
         image_height = int(image_height/scale)
+        # FFmpeg gives error when height or width is not divisible by 2, so reducing height/width by one pixel if they are odd
+        if image_width % 2 != 0:
+            image_width -= 1
+        if image_height % 2 != 0:
+            image_height -= 1
         image = cv2.resize(image, (image_width, image_height))
     font_width, font_height = font.getsize("A")
-    print(font_width, font_height)
     lab_image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
@@ -58,10 +62,7 @@ def AscifyImage(image, bg="black", res=-1, save=False, show=False):
 
     rgb_image = cv2.cvtColor(hsv_image, cv2.COLOR_HSV2RGB)
 
-    output = np.zeros_like(image)
-    if bg_code != 0:
-        output.fill(255)
-
+    output = np.full_like(image, bg_code)
     pillow_output = Image.fromarray(output)
     pillow_drawer = ImageDraw.Draw(pillow_output)
     num_rows = int(image_height / font_height)
@@ -69,10 +70,10 @@ def AscifyImage(image, bg="black", res=-1, save=False, show=False):
 
     for i in range(num_rows):
         for j in range(num_cols):
-            y_start = i*font_height
-            x_start = j*font_width
-            x_end = x_start + font_width
-            y_end = y_start + font_height
+            y_start = font_height*i
+            x_start = font_width*j
+            y_end = font_height*(i+1)
+            x_end = font_width*(j+1)
 
             intensity = np.mean(lab_image[y_start:y_end, x_start:x_end, 0])
             # i1 = np.mean(hsv_image[y_start:y_end, x_start:x_end, 1])
@@ -87,9 +88,7 @@ def AscifyImage(image, bg="black", res=-1, save=False, show=False):
             position = int((intensity/255) * (len(char_list)))-1
 
             color = np.mean(rgb_image[y_start:y_end, x_start:x_end], axis=(
-                0, 1)).astype(np.uint16)
-            for c in color:
-                c = min(c*1.2, 255)
+                0, 1)).astype(np.uint8)
             # color = (255, 255, 255)
 
             pillow_drawer.text((x_start, y_start), str(
@@ -99,25 +98,30 @@ def AscifyImage(image, bg="black", res=-1, save=False, show=False):
     output_hsv = cv2.cvtColor(output, cv2.COLOR_RGB2HSV)
     # output_hsv[..., 1] = cv2.equalizeHist(output_hsv[..., 1])
     # output_hsv[..., 2] = cv2.equalizeHist(output_hsv[..., 2])
+    # increasing the Brightness by 20%
+    output_hsv[:, :, 2] += 20
     output_bgr = cv2.cvtColor(output_hsv, cv2.COLOR_HSV2BGR)
     if save:
-        cv2.imwrite("./Outputs/Out.jpg", output_bgr)
+        cv2.imwrite("./Outputs/Out.png", output_bgr)
     if show:
         ShowImg(output_bgr)
     return output_bgr
 
 
 def AsciifyVideo(video_path, background="black", frame_skip=1, resolution=-1):
+    # Building the File Path for the Output
     out_path = './Outputs'
-    # gets the file name without extension
     file_name = os.path.splitext(os.path.basename(video_path))[0]
     in_extension = os.path.splitext(os.path.basename(video_path))[1]
     path = os.path.join(out_path, file_name)
     path_frames = os.path.join(path, 'frames')
     os.makedirs(path_frames, mode=0o777, exist_ok=True)
 
+    # Capturing the Video And Extracting Frames
     vidCap = cv2.VideoCapture(video_path)
-    for frame_idx in range(int(vidCap.get(cv2.CAP_PROP_FRAME_COUNT))):
+    NumFrames = int(vidCap.get(cv2.CAP_PROP_FRAME_COUNT))
+    FPS = vidCap.get(cv2.CAP_PROP_FPS)
+    for frame_idx in range(NumFrames):
         # Reading a frame
         ret, frame = vidCap.read()
         if ret == False:
@@ -126,13 +130,13 @@ def AsciifyVideo(video_path, background="black", frame_skip=1, resolution=-1):
             continue
         cv2.imwrite(f"{path_frames}/frame_{frame_idx+1}.jpg",
                     AscifyImage(frame, bg=background, res=resolution))
-        print(frame_idx)
-    print("Done Preprocessing")
+        print(f"Processed Frame : {frame_idx}")
+    print("Done Processing Frames")
 
-    # Assembling Video from The Produced Frames
+    # Assembling Video from the Produced Frames and Audio from the input Video
     in1 = ffmpeg.input(f'{path_frames}/frame_%d.jpg',
-                       framerate=(vidCap.get(cv2.CAP_PROP_FPS)/frame_skip))
-    # checking if audio is present in original stream
+                       framerate=(FPS/frame_skip))
+    # Checking if audio is present in original stream
     audio_present = ffmpeg.probe(video_path, select_streams='a')
     if audio_present['streams']:
         in2 = ffmpeg.input(video_path)
@@ -149,8 +153,11 @@ def AsciifyVideo(video_path, background="black", frame_skip=1, resolution=-1):
 # AscifyFrame(image=cv2.imread(
 #     "/home/arpit/Pictures/Wallpapers/11chromaticdark.png"), bg="black")
 
-# AsciifyVideo('./data/NootNoot2D.webm',background="white", resolution = 500)
-AscifyImage(cv2.imread(
-    "/home/arpit/Pictures/Wallpapers/arch-linux-wallpaper-1080p.png"), save=True, show=False)
+# AsciifyVideo('/home/arpit/Programming/Python/ASCII-Art-Gen/Asciify-Videos/data/Video_Sample.mp4',
+#              background="black", resolution=500)
+AscifyImage(cv2.imread("data/Images/arch-linux-wallpaper-1080p.png"),
+            bg="white", save=True, show=False)
+# AscifyImage(cv2.imread(
+#     "data/input2.jpg"), bg="white", save=True, show=False)
 # AscifyImage(cv2.imread(
 #     "/home/arpit/Programming/Python/ASCII-Art-Gen/data/python-logo.png"), save=True, show=False)
